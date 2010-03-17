3866bce02f4ee63e123498f29310a52facb15f81
Filipe Militão <foliveir@CMU-353860.WV.CC.CMU.EDU>
2010-03-17 16:56:14 -0400

    added mobile version of alloc()

--- src/common/ExtActiveRDMA.java
+++ src/common/ExtActiveRDMA.java
@@ -2,6 +2,7 @@ package common;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class ExtActiveRDMA implements ActiveRDMA{
 	
@@ -9,17 +10,33 @@ public abstract class ExtActiveRDMA implements ActiveRDMA{
 	 * Allocation function
 	 */
 	
+
 	//this integer is the RESERVED position for counting allocated blocks
 	//should not be used for anything else... or we will have bugs.
 	//FIXME: memory is never reclaimed! dealloc()?
-	static final int MEM_COUNTER = 0;
+	
+	//@MobileCode //TODO: use annotations to, at runtime (up)load lib code?
+	public static class Alloc{
+		static final int MEM_COUNTER = 0;
+		public static int execute(AtomicInteger[] mem, int size) {
+			int c = mem[MEM_COUNTER].get();
+			//retry until we were able to increase the claimed memory by 'size'
+			while( !mem[MEM_COUNTER].compareAndSet(c, c+size) ){
+				//failed, get the new value
+				c = mem[MEM_COUNTER].get();
+			}
+			return c;
+		}
+	}
+	
+	//TODO: should this be on the server side instead?
 	public int alloc(int size){
-		int c = r(MEM_COUNTER);
+		int c = r(Alloc.MEM_COUNTER);
 		//retry until we were able to increase the claimed memory by 'size'
-		while( cas(MEM_COUNTER,c,c+size) == 0 ){
+		while( cas(Alloc.MEM_COUNTER,c,c+size) == 0 ){
 			//failed, get the new value
-			c = r(MEM_COUNTER);
-		} //TODO: should this be on the server side instead?
+			c = r(Alloc.MEM_COUNTER);
+		}
 		return c;
 	}
 	

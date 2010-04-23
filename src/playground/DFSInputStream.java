d610bccf7cdcc711750657d8c6fe6953ebd69db0
Filipe Militão <foliveir@CMU-353860.WV.CC.CMU.EDU>
2010-04-23 16:46:15 -0400

    DFS inputstream

---
+++ src/playground/DFSInputStream.java
@@ -0,0 +1,54 @@
+package playground;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import dfs.DFS;
+
+public class DFSInputStream extends InputStream{
+
+	final static int SIZE = 512;
+
+	DFS dfs;
+	int inode, len, off;
+	byte[] buffer;
+
+	public DFSInputStream(String file, DFS dfs) throws IOException{
+		this.dfs = dfs;
+		this.inode = dfs.lookup(file);
+		this.len = dfs.getLen(this.inode);
+		this.buffer = new byte[SIZE];
+	}
+
+	@Override
+	//-1 on EOF
+	public int read() throws IOException {
+		if( off >= len )
+			return -1;
+		dfs.get(inode, buffer, off, 1);
+		off += 1;
+		return buffer[0];
+	}
+
+	@Override
+	//-1 on EOF
+	public int read(byte b[], int o, int l) {
+		if( off >= len )
+			return -1;
+		int n = Math.min(l, (len-off) );
+		l = n;
+		while( l>0 ){
+			int read_block = Math.min(l, buffer.length);
+			dfs.get(inode, buffer, off, read_block);
+			off += read_block;
+			System.arraycopy(buffer,0,b,o,read_block);
+			o += read_block;
+			l -= read_block;
+		}
+		return n;
+	}
+
+	@Override
+	public void close() throws IOException { }
+
+}

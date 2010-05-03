f9c19d918f52d1ab24ae065971ba112332d1ab81
Chris Fallin <cfallin@ece.cmu.edu>
2010-05-03 17:56:33 -0400

    redid NFS results: bypassed cache

--- test-grep.sh
+++ test-grep.sh
@@ -3,10 +3,10 @@ ROOT=$1
 RESULTS=$2
 
 for x in 0 1 2 3 4 5 6 7 8 9; do
-    cp /usr/share/dict/words $ROOT/words-$x
+#cp /usr/share/dict/words $ROOT/words-$x
     sim/timing/timing > $RESULTS/t$x
-    grep ^apple$ $ROOT/words-$x
-    grep ^zoo$ $ROOT/words-$x
-    grep ^moo$ $ROOT/words-$x
+    grep ^apple$ $ROOT/words$x
+    grep ^zoo$ $ROOT/words$x
+    grep ^moo$ $ROOT/words$x
     sim/timing/timing >> $RESULTS/t$x
 done 
